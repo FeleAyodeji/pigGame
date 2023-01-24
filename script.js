2086e57cf35374ed8c052b9bd23342f32b2d7f59
FeleAyodeji <feledeji@gmail.com>
2023-01-24 22:22:16 +0100

    rolling dice functionality

--- script.js
+++ script.js
@@ -4,9 +4,24 @@ const score0El = document.querySelector('#score--0')
 // we can also select an id by using getElementID
 const score1El = document.getElementById('score--1')
 const diceEl = document.queryselector('.dice')
+const btnNew = document.querySelector('.btn--new')
+const btnRoll = document.querySelector('.btn--roll')
+const btnOld = document.querySelector('.btn--old')
 
 //starting conditions
 score0El.textContent = 0
 score1El.textContent = 0
 
 diceEl.classList.add('hidden')
+
+//rolling dice functionality
+btnRoll.addEventListener('click', function(){
+//generating a random dice roll
+const dice = Math.trunc(Math.random()*6) + 1
+//ldisplay dice
+diceEl.classList.remove('hiddem')
+diceEl.src = 'dice-${dice}.png'
+
+
+
+})
