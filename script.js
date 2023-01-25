d5bf7a7832a060c7e9cd6e5ead8dcbce1676f90b
FeleAyodeji <feledeji@gmail.com>
2023-01-25 23:21:52 +0100

    rolling dice functionality

--- script.js
+++ script.js
@@ -11,16 +11,31 @@ const btnOld = document.querySelector('.btn--old')
 //starting conditions
 score0El.textContent = 0
 score1El.textContent = 0
-
 diceEl.classList.add('hidden')
+
+let currentScore = 0
 
 //rolling dice functionality
 btnRoll.addEventListener('click', function(){
-//generating a random dice roll
+
+// 1.generating a random dice roll
 const dice = Math.trunc(Math.random()*6) + 1
-//ldisplay dice
-diceEl.classList.remove('hiddem')
+
+//2.display dice
+diceEl.classList.remove('hidden')
 diceEl.src = 'dice-${dice}.png'
+
+//3.check for rolled 1 
+if(dice !== 1){
+//add dice to current sore
+currentScore += dice
+
+} 
+else{
+// switch to next player
+
+}
+
 
 
 
