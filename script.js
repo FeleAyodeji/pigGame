0f6ef9b5afad965042922be3d506f94045d53d67
FeleAyodeji <feledeji@gmail.com>
2023-01-29 11:28:09 +0100

    roll dice button

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 const score0El = document.querySelector('#score--0')
 // we can also select an id by using getElementID
 const score1El = document.getElementById('score--1')
+const current0El = document.getElementById('current--0')
+const current1El = document.getElementById('current--1')
 const diceEl = document.queryselector('.dice')
 const btnNew = document.querySelector('.btn--new')
 const btnRoll = document.querySelector('.btn--roll')
@@ -13,28 +15,35 @@ score0El.textContent = 0
 score1El.textContent = 0
 diceEl.classList.add('hidden')
 
+const scores = [0,0]
 let currentScore = 0
+let activePlayer = 0
 
 //rolling dice functionality
 btnRoll.addEventListener('click', function(){
 
 // 1.generating a random dice roll
 const dice = Math.trunc(Math.random()*6) + 1
+console.log(dice);
 
 //2.display dice
 diceEl.classList.remove('hidden')
-diceEl.src = 'dice-${dice}.png'
+diceEl.src = `dice-${dice}.png`
+
 
 //3.check for rolled 1 
 if(dice !== 1){
 //add dice to current sore
-currentScore += dice
+ currentScore += dice
+ document.getElementById(`current--${activePlayer}.textcontent`) = currentScore
 
-} 
-else{
-// switch to next player
+ }  else{
+ // switch to next player
+ document.getElementById(`current--${activePlayer}.textcontent`) = 0
+ activePlayer = activePlayer === 0 ? 1 : 0
+ 
 
-}
+ }
 
 
 
