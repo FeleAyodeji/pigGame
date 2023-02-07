ccfe7a80b6886f9fdc6cfff392b5c7d318f6f976
FeleAyodeji <feledeji@gmail.com>
2023-02-07 22:57:53 +0100

    fixed bug

--- script.js
+++ script.js
@@ -14,93 +14,93 @@ let btnHold = document.querySelector('.btn--hold'); // class selector for hold s
 
 current0El.textContent = 4;
 
-// let scores;
-// let currentScore;
-// let activePlayer;
-// let playing;
+let scores;
+let currentScore;
+let activePlayer;
+let playing;
 
-// let scores, currentScore, active, playing; // initial declaration of variables
+let scores, currentScore, active, playing; // initial declaration of variables
 
-// //starting conditions
-// const init = function () {
-//   scores = [0, 0];
-//   currentScore = 0;
-//   activePlayer = 0;
-//   playing = true;
+//starting conditions
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
 
-//   score0El.textContent = 0;
-//   score1El.textContent = 0;
-//   current0El.textContent = 0;
-//   current1El.textContent = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
 
-//   diceEl.classList.add('hidden'); //The classList JavaScript allows us to add, remove, replace, toggle or check whether the specified CSS class is present or not
-//   player0El.classList.remove('.player--winner');
-//   player1El.classList.remove('.player--winner');
-//   player0El.classList.add('.player--active');
-//   player1El.classList.remove('.player--active');
-// };
+  diceEl.classList.add('hidden'); //The classList JavaScript allows us to add, remove, replace, toggle or check whether the specified CSS class is present or not
+  player0El.classList.remove('.player--winner');
+  player1El.classList.remove('.player--winner');
+  player0El.classList.add('.player--active');
+  player1El.classList.remove('.player--active');
+};
 
-// init();
+init();
 
-// const switchPlayer = function () {
-//   document.getElementById(`current--${activePlayer}`).textcontent = 0;
-//   currentScore = 0;
-//   activePlayer = activePlayer === 0 ? 1 : 0;
-//   player0El.classList.toggle('player--active'); //toggle() method supports adding and removing CSS classes whether they exist or not in your array with shorter lines of code
-//   player1El.classList.toggle('player--active');
-// };
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textcontent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active'); //toggle() method supports adding and removing CSS classes whether they exist or not in your array with shorter lines of code
+  player1El.classList.toggle('player--active');
+};
 
-// //rolling dice functionality
-// btnRoll.addEventListener('click', function () {
-//   if (playing) {
-//     // 1.generating a random dice roll
-//     const dice = Math.trunc(Math.random() * 6) + 1;
+//rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    // 1.generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
 
-//     //2.display dice
-//     diceEl.classList.remove('hidden'); // it makes the dice element appear on the screen
-//     diceEl.src = `dice-${dice}.png`;
+    //2.display dice
+    diceEl.classList.remove('hidden'); // it makes the dice element appear on the screen
+    diceEl.src = `dice-${dice}.png`;
 
-//     //3.check for rolled 1
-//     if (dice !== 1) {
-//       //add dice to current sore
-//       currentScore += dice;
-//       document.getElementById(`current--${activePlayer}`).textcontent =
-//         currentScore; //it displays the currentscore of the player , provided the player doesnt roll a 1.
-//     } else {
-//       // switch to next player
-//       switchPlayer();
-//     }
-//   }
-// });
+    //3.check for rolled 1
+    if (dice !== 1) {
+      //add dice to current sore
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textcontent =
+        currentScore; //it displays the currentscore of the player , provided the player doesnt roll a 1.
+    } else {
+      // switch to next player
+      switchPlayer();
+    }
+  }
+});
 
-// //hold dice functionality
-// btnHold.addEventListener('click', function () {
-//   if (playing) {
-//     // 1. add current scoren to active player's score
-//     //score[1] = score[1] + currentScore
-//     scores[activePlayer] += currentScore;
+//hold dice functionality
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    // 1. add current scoren to active player's score
+    //score[1] = score[1] + currentScore
+    scores[activePlayer] += currentScore;
 
-//     document.getElementById(`score--${activePlayer}`).textContent =
-//       scores[activePlayer]; //this displays the sum of the player score after holding.
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer]; //this displays the sum of the player score after holding.
 
-//     // 2. check if player's score is >= 100
-//     if (scores[activePlayer] >= 100) {
-//       //finish game
-//       playing = false; // this causes the game to stop.
-//       diceEl.classList.add('hidden'); // makes the dice element disspear from the screen.
+    // 2. check if player's score is >= 100
+    if (scores[activePlayer] >= 100) {
+      //finish game
+      playing = false; // this causes the game to stop.
+      diceEl.classList.add('hidden'); // makes the dice element disspear from the screen.
 
-//       document
-//         .queryselector(`.player--${activePlayer}`)
-//         .classList.add('.player--winner');
-//       document
-//         .queryselector(`.player--${activePlayer}`)
-//         .classList.remove('.player--winner');
-//     } else {
-//       //switch to the next player
-//       switchPlayer();
-//     }
-//   }
-// });
+      document
+        .queryselector(`.player--${activePlayer}`)
+        .classList.add('.player--winner');
+      document
+        .queryselector(`.player--${activePlayer}`)
+        .classList.remove('.player--winner');
+    } else {
+      //switch to the next player
+      switchPlayer();
+    }
+  }
+});
 
-// //Reset game functionality
-// btnNew.addEventListener('click', init);
+//Reset game functionality
+btnNew.addEventListener('click', init);
